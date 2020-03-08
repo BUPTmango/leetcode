cfbe2d44dcd0a01bf4305b607bf5da4272c6c40d
Guolong Wang <13121593633@163.com>
2020-03-08 10:58:22 +0800

    2020.3.8

---
+++ src/simple_level/jianzhi_offer/ConstructArr.java
@@ -0,0 +1,81 @@
+package simple_level.jianzhi_offer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 面试题66. 构建乘积数组
+ * 给定一个数组 A[0,1,…,n-1]，请构建一个数组 B[0,1,…,n-1]，其中 B 中的元素 B[i]=A[0]×A[1]×…×A[i-1]×A[i+1]×…×A[n-1]。不能使用除法。
+ *
+ * ?
+ *
+ * 示例:
+ *
+ * 输入: [1,2,3,4,5]
+ * 输出: [120,60,40,30,24]
+ * ?
+ *
+ * 提示：
+ *
+ * 所有元素乘积之和不会溢出 32 位整数
+ * a.length <= 100000
+ *
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2020/3/8 9:50 上午
+ */
+public class ConstructArr {
+    /**
+     * 自己写的方法 超过时间限制 不通过
+     * @param a
+     * @return
+     */
+    public int[] constructArr(int[] a) {
+        int[] result = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            int product = 1;
+            for (int j = 0; j < a.length; j++) {
+                if (j == i) {
+                    continue;
+                } else {
+                    product *= a[j];
+                }
+            }
+            result[i] = product;
+        }
+        return result;
+    }
+
+    /**
+     * 从左往右遍历累乘，结果保存在数组 retret 中，此时 ret[i]ret[i] 表示，A[i]A[i] 左边所有元素的乘积
+     * 然后从右往左遍历累乘，获取A[i]A[i] 右边所有元素的乘积
+     * 两边遍历之后得到的 retret，就是最终结果
+     *
+     * @param a
+     * @return
+     */
+    public int[] constructArr_better(int[] a) {
+        int length = a.length;
+        int[] result = new int[length];
+        int left = 1;
+        for (int i = 0; i < length; i++) {
+            result[i] = left;
+            left *= a[i];
+        }
+        int right = 1;
+        for (int j = length - 1; j >= 0; j--) {
+            result[j] *= right;
+            right *= a[j];
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ConstructArr c = new ConstructArr();
+        int[] result = c.constructArr(new int[]{1,2,3,4,5});
+        for (int i : result) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/simple_level/jianzhi_offer/IsSymmetric.java
@@ -0,0 +1,105 @@
+package simple_level.jianzhi_offer;
+
+import data_structure.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 面试题28. 对称的二叉树
+ * 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+ *
+ * 例如，二叉树?[1,2,2,3,4,4,3] 是对称的。
+ *
+ * ? ? 1
+ * ? ?/ \
+ * ? 2 ? 2
+ * ?/ \ / \
+ * 3 ?4 4 ?3
+ * 但是下面这个?[1,2,2,null,3,null,3] 则不是镜像对称的:
+ *
+ * ? ? 1
+ * ? ?/ \
+ * ? 2 ? 2
+ * ? ?\ ? \
+ * ? ?3 ? ?3
+ *
+ * ?
+ *
+ * 示例 1：
+ *
+ * 输入：root = [1,2,2,3,4,4,3]
+ * 输出：true
+ * 示例 2：
+ *
+ * 输入：root = [1,2,2,null,3,null,3]
+ * 输出：false
+ * ?
+ *
+ * 限制：
+ *
+ * 0 <= 节点个数 <= 1000
+ *
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2020/3/8 9:06 上午
+ */
+public class IsSymmetric {
+    /**
+     * 递归方法
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isMirror(root.left, root.right);
+    }
+    private boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    }
+
+    /**
+     * 迭代方法
+     * 使用队列，每次放进去两个元素进行比较
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric_notRecommend(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root.left);
+        q.add(root.right);
+        while (!q.isEmpty()) {
+            TreeNode left = q.poll();
+            TreeNode right = q.poll();
+            if (left == null && right == null) {
+                continue;
+            }
+            if (left == null || right == null) {
+                return false;
+            }
+            if (left.val != right.val) {
+                return false;
+            }
+            q.add(left.left);
+            q.add(right.right);
+            q.add(left.right);
+            q.add(right.left);
+        }
+        return true;
+    }
+
+}
