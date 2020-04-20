542576f8fb2c2b6398f37a6aef6579fb4c5f7530
Guolong Wang <13121593633@163.com>
2020-04-20 14:13:17 +0800

    2020.4.20

---
+++ src/middle_level/NumIslands.java
@@ -0,0 +1,128 @@
+package middle_level;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 200. 岛屿数量
+ * 给你一个由?'1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ *
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+ *
+ * 此外，你可以假设该网格的四条边均被水包围。
+ *
+ * 示例 1:
+ *
+ * 输入:
+ * 11110
+ * 11010
+ * 11000
+ * 00000
+ * 输出:?1
+ * 示例?2:
+ *
+ * 输入:
+ * 11000
+ * 11000
+ * 00100
+ * 00011
+ * 输出: 3
+ * 解释: 每座岛屿只能由水平和/或竖直方向上相邻的陆地连接而成。
+ *
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2020/4/20 1:38 下午
+ */
+public class NumIslands {
+    /**
+     * 深度优先算法
+     * 找到1和相邻的1，把他们变成0，并且岛屿数加1
+     * @param grid
+     * @param r
+     * @param c
+     */
+    void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
+        }
+        grid[r][c] = '0'; // 表示当前已经遍历过
+        // 遍历四个方向
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    // 找到1 进行操作
+                    ++num_islands;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+
+        return num_islands;
+    }
+
+    /**
+     * 广度优先搜索
+     * 找到1放进队列中，并设置当前为0
+     * 直到队列为空
+     * @param grid
+     * @return
+     */
+    public int numIslands_another(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    grid[r][c] = '0';
+                    Queue<Integer> neighbors = new LinkedList<>();
+                    // 找到1放进队列
+                    neighbors.add(r * nc + c);
+                    while (!neighbors.isEmpty()) {
+                        int id = neighbors.remove();
+                        int row = id / nc;
+                        int col = id % nc;
+                        // 找到周围的1，放进队列
+                        if (row - 1 >= 0 && grid[row-1][col] == '1') {
+                            neighbors.add((row-1) * nc + col);
+                            grid[row-1][col] = '0';
+                        }
+                        if (row + 1 < nr && grid[row+1][col] == '1') {
+                            neighbors.add((row+1) * nc + col);
+                            grid[row+1][col] = '0';
+                        }
+                        if (col - 1 >= 0 && grid[row][col-1] == '1') {
+                            neighbors.add(row * nc + col-1);
+                            grid[row][col-1] = '0';
+                        }
+                        if (col + 1 < nc && grid[row][col+1] == '1') {
+                            neighbors.add(row * nc + col+1);
+                            grid[row][col+1] = '0';
+                        }
+                    }
+                }
+            }
+        }
+
+        return num_islands;
+    }
+}
