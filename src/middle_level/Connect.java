50e9f568632c17f9e6a38ab2702a85a8cfe73b2b
Guolong Wang <13121593633@163.com>
2020-04-27 16:05:50 +0800

    2020.4.26

---
+++ src/middle_level/Connect.java
@@ -0,0 +1,54 @@
+package middle_level;
+
+import data_structure.Node;
+
+/**
+ * 116. 填充每个节点的下一个右侧节点指针
+ * 给定一个完美二叉树，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+ *
+ * struct Node {
+ *   int val;
+ *   Node *left;
+ *   Node *right;
+ *   Node *next;
+ * }
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ *
+ * 初始状态下，所有?next 指针都被设置为 NULL。
+ *
+ * ?
+ *
+ * 示例：
+ *
+ *
+ *
+ * 输入：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":{"$id":"6","left":null,"next":null,"right":null,"val":6},"next":null,"right":{"$id":"7","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}
+ *
+ * 输出：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":{"$id":"4","left":null,"next":{"$id":"5","left":null,"next":{"$id":"6","left":null,"next":null,"right":null,"val":7},"right":null,"val":6},"right":null,"val":5},"right":null,"val":4},"next":{"$id":"7","left":{"$ref":"5"},"next":null,"right":{"$ref":"6"},"val":3},"right":{"$ref":"4"},"val":2},"next":null,"right":{"$ref":"7"},"val":1}
+ *
+ * 解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。
+ *
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2020/4/26 12:01 下午
+ */
+public class Connect {
+    /**
+     * 每个 node 左子树的 next , 就是 node 的右子树
+     * 每个 node 右子树的 next, 就是 node next 的 左子树
+     * @param root
+     * @return
+     */
+    public Node connect(Node root) {
+        dfs(root, null);
+        return root;
+    }
+
+    private void dfs(Node node, Node next) {
+        if(node != null) {
+            node.next = next;
+            dfs(node.left, node.right);
+            dfs(node.right, node.next != null ? node.next.left : null);
+        }
+    }
+}
