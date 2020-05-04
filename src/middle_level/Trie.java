db3a171df8c12f35bfcd62b7bdabf4657a8972b3
Guolong Wang <13121593633@163.com>
2020-05-04 10:55:09 +0800

    2020.5.4

---
+++ src/middle_level/Trie.java
@@ -0,0 +1,61 @@
+package middle_level;
+
+/**
+ * 208. 实现 Trie (前缀树)
+ * 实现一个 Trie (前缀树)，包含?insert,?search, 和?startsWith?这三个操作。
+ *
+ * 示例:
+ *
+ * Trie trie = new Trie();
+ *
+ * trie.insert("apple");
+ * trie.search("apple");   // 返回 true
+ * trie.search("app");     // 返回 false
+ * trie.startsWith("app"); // 返回 true
+ * trie.insert("app");
+ * trie.search("app");     // 返回 true
+ * 说明:
+ *
+ * 你可以假设所有的输入都是由小写字母?a-z?构成的。
+ * 保证所有输入均为非空字符串。
+ *
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2020/5/4 10:08 上午
+ */
+public class Trie {
+    private boolean is_string = false;
+    private Trie next[] = new Trie[26];
+
+    public Trie(){}
+
+    public void insert(String word){//插入单词
+        Trie root=this;
+        char w[]=word.toCharArray();
+        for(int i=0;i<w.length;++i){
+            if(root.next[w[i]-'a']==null)root.next[w[i]-'a']=new Trie();
+            root=root.next[w[i]-'a'];
+        }
+        root.is_string=true;
+    }
+
+    public boolean search(String word){//查找单词
+        Trie root=this;
+        char w[]=word.toCharArray();
+        for(int i=0;i<w.length;++i){
+            if(root.next[w[i]-'a']==null)return false;
+            root=root.next[w[i]-'a'];
+        }
+        return root.is_string;
+    }
+
+    public boolean startsWith(String prefix){//查找前缀
+        Trie root=this;
+        char p[]=prefix.toCharArray();
+        for(int i=0;i<p.length;++i){
+            if(root.next[p[i]-'a']==null)return false;
+            root=root.next[p[i]-'a'];
+        }
+        return true;
+    }
+}
