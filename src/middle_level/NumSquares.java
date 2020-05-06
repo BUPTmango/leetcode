2b777293cef84cf4f5b72e06eca26593e6d34e5a
Guolong Wang <13121593633@163.com>
2020-05-07 00:23:42 +0800

    2020.5.6

--- src/middle_level/NumSquares.java
+++ src/middle_level/NumSquares.java
@@ -1,5 +1,9 @@
 package middle_level;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 279. 完全平方数
  * 给定正整数?n，找到若干个完全平方数（比如?1, 4, 9, 16, ...）使得它们的和等于 n。你需要让组成和的完全平方数的个数最少。
@@ -41,5 +45,47 @@ public class NumSquares {
             }
         }
         return dp[n];
+    }
+
+    /**
+     * 广度优先搜索
+     * 首先，我们准备小于给定数字 n 的完全平方数列表（即 square_nums）。
+     * 然后创建 queue 遍历，该变量将保存所有剩余项在每个级别的枚举。
+     * 在主循环中，我们迭代 queue 变量。在每次迭代中，我们检查余数是否是一个完全平方数。
+     * 如果余数不是一个完全平方数，就用其中一个完全平方数减去它，得到一个新余数，然后将新余数添加到 next_queue 中，以进行下一级的迭代。
+     * 一旦遇到一个完全平方数的余数，我们就会跳出循环，这也意味着我们找到了解。
+     * @param n
+     * @return
+     */
+    public int numSquares_BFS(int n) {
+
+        // 将可能的平方数放到list中
+        ArrayList<Integer> square_nums = new ArrayList<>();
+        for (int i = 1; i * i <= n; ++i) {
+            square_nums.add(i * i);
+        }
+
+        Set<Integer> queue = new HashSet<>();
+        queue.add(n);
+
+        int level = 0;
+        while (queue.size() > 0) {
+            level += 1;
+            Set<Integer> next_queue = new HashSet<Integer>();
+
+            for (Integer remainder : queue) {
+                for (Integer square : square_nums) {
+                    if (remainder.equals(square)) {
+                        return level;
+                    } else if (remainder < square) {
+                        break;
+                    } else {
+                        next_queue.add(remainder - square);
+                    }
+                }
+            }
+            queue = next_queue;
+        }
+        return level;
     }
 }
