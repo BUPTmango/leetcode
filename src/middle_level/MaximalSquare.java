c499975d88053a4955e64aaa286fbcdbca7a07ed
Guolong Wang <13121593633@163.com>
2020-05-10 14:14:45 +0800

    2020.5.10

---
+++ src/middle_level/MaximalSquare.java
@@ -0,0 +1,48 @@
+package middle_level;
+
+/**
+ * 221. 最大正方形
+ * 在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。
+ *
+ * 示例:
+ *
+ * 输入:
+ *
+ * 1 0 1 0 0
+ * 1 0 1 1 1
+ * 1 1 1 1 1
+ * 1 0 0 1 0
+ *
+ * 输出: 4
+ *
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2020/5/10 1:55 下午
+ */
+public class MaximalSquare {
+    /**
+     * 动态规划
+     * 如果当前值是1，比较上方值，左方值，左上方三个值的最小值，并+1
+     * @param matrix
+     * @return
+     */
+    public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return 0;
+        }
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] dp = new int[rows + 1][cols + 1];
+        int max = 0;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1],
+                            Math.min(dp[i][j - 1], dp[i - 1][j])) + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+        return max * max;
+    }
+}
