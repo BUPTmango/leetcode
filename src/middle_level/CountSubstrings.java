95245a3bcc5487aded257f2c339f08a0e1ca809e
Guolong Wang <13121593633@163.com>
2020-06-08 11:07:56 +0800

    2020.6.8

---
+++ src/middle_level/CountSubstrings.java
@@ -0,0 +1,43 @@
+package middle_level;
+
+/**
+ * 647. 回文子串
+ * 给定一个字符串，你的任务是计算这个字符串中有多少个回文子串。
+ *
+ * 具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被计为是不同的子串。
+ *
+ * 示例 1:
+ *
+ * 输入: "abc"
+ * 输出: 3
+ * 解释: 三个回文子串: "a", "b", "c".
+ * 示例 2:
+ *
+ * 输入: "aaa"
+ * 输出: 6
+ * 说明: 6个回文子串: "a", "a", "a", "aa", "aa", "aaa".
+ *
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2020/6/8 10:05 上午
+ */
+public class CountSubstrings {
+    public int countSubstrings(String s) {
+        // 中心扩展法
+        int ans = 0;
+        for (int center = 0; center < 2 * s.length() - 1; center++) {
+            // left和right指针和中心点的关系是？
+            // 首先是left，有一个很明显的2倍关系的存在，其次是right，
+            // 可能和left指向同一个（偶数时），也可能往后移动一个（奇数）
+            int left = center / 2;
+            int right = left + center % 2;
+
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                ans++;
+                left--;
+                right++;
+            }
+        }
+        return ans;
+    }
+}
