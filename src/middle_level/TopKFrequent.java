e913e882a78a57e4eeff4301a5993a01a258ca37
Guolong Wang <13121593633@163.com>
2020-06-10 13:26:24 +0800

    2020.6.10

---
+++ src/middle_level/TopKFrequent.java
@@ -0,0 +1,50 @@
+package middle_level;
+
+import java.util.*;
+
+/**
+ * 347. 前 K 个高频元素
+ * 给定一个非空的整数数组，返回其中出现频率前?k?高的元素。
+ *
+ * ?
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [1,1,1,2,2,3], k = 2
+ * 输出: [1,2]
+ * 示例 2:
+ *
+ * 输入: nums = [1], k = 1
+ * 输出: [1]
+ *
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2020/6/10 11:45 上午
+ */
+public class TopKFrequent {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        // build hash map : character and how often it appears
+        HashMap<Integer, Integer> count = new HashMap();
+        for (int n: nums) {
+            count.put(n, count.getOrDefault(n, 0) + 1);
+        }
+
+        // init heap 'the less frequent element first'
+        PriorityQueue<Integer> heap =
+                new PriorityQueue<Integer>(Comparator.comparingInt(count::get));
+
+        // keep k top frequent elements in the heap
+        for (int n: count.keySet()) {
+            heap.add(n);
+            if (heap.size() > k)
+                heap.poll();
+        }
+
+        // build output list
+        List<Integer> top_k = new LinkedList();
+        while (!heap.isEmpty())
+            top_k.add(heap.poll());
+        Collections.reverse(top_k);
+        return top_k;
+    }
+}
