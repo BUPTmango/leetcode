bd0cbfa111114ca381ed205a6e36381cbae2f493
Guolong Wang <13121593633@163.com>
2020-08-12 09:10:31 +0800

    2020.8.12

---
+++ src/middle_level/jianzhi_offer/CuttingRopeII.java
@@ -0,0 +1,44 @@
+package middle_level.jianzhi_offer;
+
+/**
+ * 剑指 Offer 14- II. 剪绳子 II
+ * 给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m?段（m、n都是整数，n>1并且m>1），每段绳子的长度记为 k[0],k[1]...k[m - 1] 。请问 k[0]*k[1]*...*k[m - 1] 可能的最大乘积是多少？例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+ *
+ * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+ *
+ * ?
+ *
+ * 示例 1：
+ *
+ * 输入: 2
+ * 输出: 1
+ * 解释: 2 = 1 + 1, 1 × 1 = 1
+ * 示例?2:
+ *
+ * 输入: 10
+ * 输出: 36
+ * 解释: 10 = 3 + 3 + 4, 3 ×?3 ×?4 = 36
+ *
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2020/8/4 5:21 下午
+ */
+public class CuttingRopeII {
+    public int cuttingRope(int n) {
+        if(n == 2) {
+            return 1;
+        }
+        if(n == 3){
+            return 2;
+        }
+        int mod = (int)1e9 + 7;
+        long res = 1;
+        // 尽可能多地切3的片段，我们可以以n>4来作为循环跳出点
+        while(n > 4) {
+            res *= 3;
+            res %= mod;
+            n -= 3;
+        }
+        return (int)(res * n % mod);
+    }
+}
