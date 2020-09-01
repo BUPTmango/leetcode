4740e3b8125ef49bef9d6a577c3e3be1a28afcbc
Guolong Wang <13121593633@163.com>
2020-09-01 19:35:12 +0800

    2020.9.1

---
+++ src/simple_level/Intersection.java
@@ -0,0 +1,60 @@
+package simple_level;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+/**
+ * 349. 两个数组的交集
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ *
+ * ?
+ *
+ * 示例 1：
+ *
+ * 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出：[2]
+ * 示例 2：
+ *
+ * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出：[9,4]
+ *
+ *
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2020/9/1 2:38 下午
+ */
+public class Intersection {
+    public int[] set_intersection(HashSet<Integer> set1, HashSet<Integer> set2) {
+        int [] output = new int[set1.size()];
+        int idx = 0;
+        for (Integer s : set1) {
+            if (set2.contains(s)) {
+                output[idx++] = s;
+            }
+        }
+
+        return Arrays.copyOf(output, idx);
+    }
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        // 首先将两个数组都转换成set
+        HashSet<Integer> set1 = new HashSet<>();
+        for (Integer n : nums1) {
+            set1.add(n);
+        }
+        HashSet<Integer> set2 = new HashSet<>();
+        for (Integer n : nums2) {
+            set2.add(n);
+        }
+
+        // 迭代较小的集合，检查其中的每个元素是否同样存在于较大的集合中
+        if (set1.size() < set2.size()) {
+            return set_intersection(set1, set2);
+        } else {
+            return set_intersection(set2, set1);
+        }
+    }
+
+}
