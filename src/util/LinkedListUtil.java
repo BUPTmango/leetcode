f2db5e3b75aa5bf81a43bee31cc74f87f0dbb18b
Guolong Wang <13121593633@163.com>
2020-09-25 09:33:44 +0800

    修改LinkedListUtil

--- src/util/LinkedListUtil.java
+++ src/util/LinkedListUtil.java
@@ -35,7 +35,7 @@ public class LinkedListUtil {
     public static void print(ListNode head) {
         ListNode cur = head;
         while (cur != null) {
-            System.out.print(cur.val + "  ->  ");
+            System.out.print(cur.val + " -> ");
             cur = cur.next;
         }
         System.out.println("NULL");
