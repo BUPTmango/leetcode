8325479eefa9469cfd5dc9c970eacc6cd0dd6097
Guolong Wang <13121593633@163.com>
2020-10-05 21:57:10 +0800

    《玩转算法面试视频例题》二叉树 递归

--- src/simple_level/MinDepth.java
+++ src/simple_level/MinDepth.java
@@ -3,22 +3,23 @@ package simple_level;
 import data_structure.TreeNode;
 
 /**
+ * 《玩转算法面试视频例题》二叉树 递归
  * 111. 二叉树的最小深度
  * 给定一个二叉树，找出其最小深度。
- *
+ * <p>
  * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
- *
+ * <p>
  * 说明:?叶子节点是指没有子节点的节点。
- *
+ * <p>
  * 示例:
- *
+ * <p>
  * 给定二叉树?[3,9,20,null,null,15,7],
- *
- *     3
- *    / \
- *   9  20
- *     /  \
- *    15   7
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
  * 返回它的最小深度 ?2.
  *
  * @author Wang Guolong
@@ -27,17 +28,23 @@ import data_structure.TreeNode;
  */
 public class MinDepth {
     public int minDepth(TreeNode root) {
-        if(root == null) return 0;
-        //这道题递归条件里分为三种情况
-        //1.左孩子和有孩子都为空的情况，说明到达了叶子节点，直接返回1即可
-        if(root.left == null && root.right == null) return 1;
-        //2.如果左孩子和右孩子其中一个为空，那么需要返回比较大的那个孩子的深度
+        if (root == null) {
+            return 0;
+        }
+        // 这道题递归条件里分为三种情况
+        // 1.左孩子和有孩子都为空的情况，说明到达了叶子节点，直接返回1即可
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        // 2.如果左孩子和右孩子其中一个为空，那么需要返回比较大的那个孩子的深度
         int m1 = minDepth(root.left);
         int m2 = minDepth(root.right);
-        //这里其中一个节点为空，说明m1和m2有一个必然为0，所以可以返回m1 + m2 + 1;
-        if(root.left == null || root.right == null) return m1 + m2 + 1;
+        // 这里其中一个节点为空，说明m1和m2有一个必然为0，所以可以返回m1 + m2 + 1;
+        if (root.left == null || root.right == null) {
+            return m1 + m2 + 1;
+        }
 
-        //3.最后一种情况，也就是左右孩子都不为空，返回最小深度+1即可
-        return Math.min(m1,m2) + 1;
+        // 3.最后一种情况，也就是左右孩子都不为空，返回最小深度+1即可
+        return Math.min(m1, m2) + 1;
     }
 }
