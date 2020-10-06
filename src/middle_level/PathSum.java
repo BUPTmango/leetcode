36817fdfff592e077396333c5a7eb8a8aba7b982
Guolong Wang <13121593633@163.com>
2020-10-06 20:27:38 +0800

    《玩转算法面试视频例题》二叉树 递归终止条件

---
+++ src/middle_level/PathSum.java
@@ -0,0 +1,69 @@
+package middle_level;
+
+import data_structure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 《玩转算法面试视频例题》二叉树 递归终止条件
+ * 113. 路径总和 II
+ * 给定一个二叉树和一个目标和，找到所有从根节点到叶子节点路径总和等于给定目标和的路径。
+ * <p>
+ * 说明:?叶子节点是指没有子节点的节点。
+ * <p>
+ * 示例:
+ * 给定如下二叉树，以及目标和?sum = 22，
+ * <p>
+ * 5
+ * / \
+ * 4   8
+ * /   / \
+ * 11  13  4
+ * /  \    / \
+ * 7    2  5   1
+ * 返回:
+ * <p>
+ * [
+ * [5,4,11,2],
+ * [5,8,4,5]
+ * ]
+ *
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2020/10/6 8:04 下午
+ */
+public class PathSum {
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        recur(root, sum);
+        return res;
+    }
+
+    /**
+     * 路径更新： 将当前节点值 root.val 加入路径 path ；
+     * 目标值更新： tar = tar - root.val（即目标值 tar 从 sum 减至 00 ）；
+     * 路径记录： 当 ① root 为叶节点 且 ② 路径和等于目标值 ，则将此路径 path 加入 res 。
+     * 先序遍历： 递归左 / 右子节点。
+     * 路径恢复： 向上回溯前，需要将当前节点从路径 path 中删除，即执行 path.pop() 。
+     * @param root
+     * @param tar
+     */
+    void recur(TreeNode root, int tar) {
+        if(root == null) {
+            return;
+        }
+        path.add(root.val);
+        tar -= root.val;
+        // 找到了路径和并且是叶子节点 就不需要继续遍历
+        if(tar == 0 && root.left == null && root.right == null) {
+            res.add(new ArrayList<>(path));
+        }
+        // 遍历左子树
+        recur(root.left, tar);
+        // 遍历右子树
+        recur(root.right, tar);
+        path.remove(path.size() - 1);
+    }
+}
--- src/simple_level/BinaryTreePaths.java
+++ src/simple_level/BinaryTreePaths.java
@@ -6,23 +6,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * 《玩转算法面试视频例题》二叉树 递归终止条件
  * 257. 二叉树的所有路径
  * 给定一个二叉树，返回所有从根节点到叶子节点的路径。
- *
+ * <p>
  * 说明:?叶子节点是指没有子节点的节点。
- *
+ * <p>
  * 示例:
- *
+ * <p>
  * 输入:
- *
- *    1
- *  /   \
+ * <p>
+ * 1
+ * /   \
  * 2     3
- *  \
- *   5
- *
+ * \
+ * 5
+ * <p>
  * 输出: ["1->2->5", "1->3"]
- *
+ * <p>
  * 解释: 所有根节点到叶子节点的路径为: 1->2->5, 1->3
  *
  * @author Wang Guolong
@@ -30,33 +31,32 @@ import java.util.List;
  * @date 2020/8/14 12:20 下午
  */
 public class BinaryTreePaths {
+    private List<String> paths = new ArrayList();
     /**
      * 如果当前的节点不是叶子节点，则在当前的路径末尾添加该节点，并递归遍历该节点的每一个孩子节点。
      * 如果当前的节点是叶子节点，则在当前的路径末尾添加该节点后，就得到了一条从根节点到叶子节点的路径，可以把该路径加入到答案中。
      *
      * @param root
      * @param path
-     * @param paths
      */
-    public void construct_paths(TreeNode root, String path, ArrayList<String> paths) {
+    private void construct_paths(TreeNode root, String path) {
         if (root != null) {
             path += Integer.toString(root.val);
-            // 当前节点是叶子节点
-            if ((root.left == null) && (root.right == null)) {
+            // 当前节点是叶子节点 这才是递归终止条件
+            if (root.left == null && root.right == null) {
                 // 把路径加入到答案中
                 paths.add(path);
             } else {
                 // 当前节点不是叶子节点，继续递归遍历
                 path += "->";
-                construct_paths(root.left, path, paths);
-                construct_paths(root.right, path, paths);
+                construct_paths(root.left, path);
+                construct_paths(root.right, path);
             }
         }
     }
 
     public List<String> binaryTreePaths(TreeNode root) {
-        ArrayList<String> paths = new ArrayList();
-        construct_paths(root, "", paths);
+        construct_paths(root, "");
         return paths;
     }
 }
