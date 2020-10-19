2d22adbc4a81fb5af6323fae30e3d41cc1c09120
Guolong Wang <13121593633@163.com>
2020-10-19 20:34:15 +0800

    《玩转算法面试视频例题》动态规划 背包问题

--- src/middle_level/CanPartition.java
+++ src/middle_level/CanPartition.java
@@ -1,6 +1,7 @@
 package middle_level;
 
 /**
+ * 《玩转算法面试视频例题》动态规划 背包问题
  * 416. 分割等和子集
  * 给定一个只包含正整数的非空数组。是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
  *
@@ -29,6 +30,11 @@ package middle_level;
  * @date 2020/10/11 5:57 下午
  */
 public class CanPartition {
+    /**
+     * 典型的背包问题 在n个物品中选出一定物品填满sum/2的背包
+     * @param nums
+     * @return
+     */
     public boolean canPartition(int[] nums) {
         int len = nums.length;
         if (len == 0) {
@@ -40,33 +46,26 @@ public class CanPartition {
         }
 
         // 特判：如果是奇数，就不符合要求
-        if ((sum & 1) == 1) {
+        if (sum % 2 != 0) {
             return false;
         }
 
-        int target = sum / 2;
-        // 创建二维状态数组，行：物品索引，列：容量（包括 0）
-        boolean[][] dp = new boolean[len][target + 1];
+        int capacity = sum / 2;
+        boolean[] dp = new boolean[capacity + 1];
 
-        // 先填表格第 0 行，第 1 个数只能让容积为它自己的背包恰好装满
-        if (nums[0] <= target) {
-            dp[0][nums[0]] = true;
+        for (int i = 0; i <= capacity; i++) {
+            // 初始化只考虑nums[0]这一个元素的时候，要是正好这一个元素就装满了背包，就为true，否则为false
+            dp[i] = (nums[0] == i);
         }
-        // 再填表格后面几行
-        for (int i = 1; i < len; i++) {
-            for (int j = 0; j <= target; j++) {
-                // 直接从上一行先把结果抄下来，然后再修正
-                dp[i][j] = dp[i - 1][j];
 
-                if (nums[i] == j) {
-                    dp[i][j] = true;
-                    continue;
-                }
-                if (nums[i] < j) {
-                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]];
-                }
+        // 考虑nums[i]元素 在背包j容量下的情况
+        for (int i = 1; i < len; i++) {
+            for (int j = capacity; j >= nums[i]; j--) {
+                // 考虑不使用和使用新添加的nums[i]元素两种情况
+                dp[j] = dp[j] || dp[j - nums[i]];
             }
         }
-        return dp[len - 1][target];
+
+        return dp[capacity];
     }
 }
