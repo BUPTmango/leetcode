cd024c855b05ce5a519a30c1cc63fc28a76f51d7
Guolong Wang <13121593633@163.com>
2020-11-16 09:49:23 +0800

    week_215

--- src/weekly_contest/week_215/MinOperations.java
+++ src/weekly_contest/week_215/MinOperations.java
@@ -4,15 +4,10 @@ import java.util.Arrays;
 
 /**
  * 5602. 将 x 减到 0 的最小操作数 显示英文描述
- * 通过的用户数462
- * 尝试过的用户数1051
- * 用户总通过次数463
- * 用户总提交次数2276
- * 题目难度Medium
+ *
  * 给你一个整数数组 nums 和一个整数 x 。每一次操作时，你应当移除数组 nums 最左边或最右边的元素，然后从 x 中减去该元素的值。请注意，需要 修改 数组以供接下来的操作使用。
  *
  * 如果可以将 x 恰好 减到 0 ，返回 最小操作数 ；否则，返回 -1 。
- *
  *
  * 示例 1：
  *
@@ -70,6 +65,38 @@ public class MinOperations {
         visited[left][right] = true;
         backtrack(nums, left + 1, right, x - nums[left], count + 1);
         backtrack(nums, left, right - 1, x - nums[right], count + 1);
+    }
+
+    /**
+     * 滑动窗口
+     *
+     * @param nums
+     * @param x
+     * @return
+     */
+    public int minOperations_better(int[] nums, int x) {
+        // 使用滑动窗口找中间最长的片段使得sum(片段)=sum(nums)-x
+        int maxPart = -1;
+        int sum = Arrays.stream(nums).sum();
+        int currentSum = 0;
+        int left = 0;
+        int right = 0;
+        while (left < nums.length) {
+            // 如果右边未到尽头，不断先向右探测片段，如果大于目标sum-x则左边移动直到结束
+            if (right < nums.length) {
+                currentSum += nums[right++];
+            }
+            while (currentSum > sum - x && left < nums.length) {
+                currentSum -= nums[left++];
+            }
+            if (currentSum == sum - x) {
+                maxPart = Math.max(maxPart, right - left);
+            }
+            if (right == nums.length) {
+                left++;
+            }
+        }
+        return maxPart == -1 ? -1 : nums.length - maxPart;
     }
 
     public static void main(String[] args) {
