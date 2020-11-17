82b8add93995e1d40c88d79b5aa98effbf779ec2
Guolong Wang <13121593633@163.com>
2020-11-17 10:32:44 +0800

    快速选择算法

--- src/middle_level/FindKthLargest.java
+++ src/middle_level/FindKthLargest.java
@@ -2,6 +2,7 @@ package middle_level;
 
 import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.Random;
 
 /**
  * 《玩转算法面试视频例题》
@@ -34,5 +35,82 @@ public class FindKthLargest {
             }
         }
         return heap.poll();
+    }
+
+    /**
+     * 快速选择算法
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int findKthLargest_quick_select(int[] nums, int k) {
+        // 首先随机打乱数组
+        shuffle(nums);
+        int lo = 0, hi = nums.length - 1;
+        // 索引转化 转换成升序的第k索引
+        k = nums.length - k;
+        while (lo <= hi) {
+            // 在 nums[lo..hi] 中选一个分界点
+            int p = partition(nums, lo, hi);
+            if (p < k) {
+                // 第 k 大的元素在 nums[p+1..hi] 中
+                lo = p + 1;
+            } else if (p > k) {
+                // 第 k 大的元素在 nums[lo..p-1] 中
+                hi = p - 1;
+            } else {
+                // 找到第 k 大元素
+                return nums[p];
+            }
+        }
+        return -1;
+    }
+
+    private void shuffle(int[] nums) {
+        int n = nums.length;
+        Random rand = new Random();
+        for (int i = 0 ; i < n; i++) {
+            // 从 i 到最后随机选一个元素
+            int r = i + rand.nextInt(n - i);
+            swap(nums, i, r);
+        }
+    }
+
+    private int partition(int[] nums, int lo, int hi) {
+        if (lo == hi) {
+            return lo;
+        }
+        // 默认将nums[lo]作为默认分界点 pivot
+        int pivot = nums[lo];
+        // j = hi + 1 因为while会先执行 --
+        int i = lo, j = hi + 1;
+        while (true) {
+            // 保证 nums[lo...i] 都小于 pivot
+            while (nums[++i] < pivot) {
+                if (i == hi) {
+                    break;
+                }
+            }
+            // 保证 nums[j...hi] 都大于 pivot
+            while (nums[--j] > pivot) {
+                if (j == lo) {
+                    break;
+                }
+            }
+            if (i >= j) {
+                break;
+            }
+            // 保证 nums[lo...i] < pivot < nums[j...hi]
+            swap(nums, i, j);
+        }
+        // 将pivot交换到正确的位置
+        swap(nums, j, lo);
+        return j;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
