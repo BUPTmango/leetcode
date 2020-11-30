45bd8d2c27ad6ee4fca5001ea929e393994698ab
Guolong Wang <13121593633@163.com>
2020-11-30 10:48:53 +0800

    week_217

--- src/weekly_contest/week_217/MostCompetitive.java
+++ src/weekly_contest/week_217/MostCompetitive.java
@@ -1,5 +1,8 @@
 package weekly_contest.week_217;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 /**
  * 5614. 找出最具竞争力的子序列
  * 给你一个整数数组 nums 和一个正整数 k ，返回长度为 k 且最具 竞争力 的 nums 子序列。
@@ -29,6 +32,59 @@ package weekly_contest.week_217;
  */
 public class MostCompetitive {
     public int[] mostCompetitive(int[] nums, int k) {
-        return null;
+        int[] res = new int[k];
+        boolean isIncre = true;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1]){
+                isIncre = false;
+                break;
+            }
+        }
+        if (isIncre) {
+            res = Arrays.copyOfRange(nums, 0, k);
+            return res;
+        }
+
+        // k个数字 寻找k次
+        int start = 0, end = nums.length - k;
+        for (int i = 0; i < k; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = start; j <= end; j++) {
+                if (nums[j] < min) {
+                    min = nums[j];
+                    // 更新start索引
+                    start = j + 1;
+                }
+            }
+            res[i] = min;
+            // 更新end索引
+            end++;
+        }
+        return res;
+    }
+
+    public int[] mostCompetitive_better(int[] nums, int k) {
+        int n = nums.length;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            // 先pop 判断剩余数组元素能不能凑够k
+            while (!stack.isEmpty() && stack.peek() > nums[i] && n - i + stack.size() > k) {
+                stack.pop();
+            }
+            // 再添加
+            stack.push(nums[i]);
+        }
+        // stack变为数组
+        int length = stack.size();
+        int[] array = new int[length];
+        for (int i = length - 1; i >= 0; i--) {
+            array[i] = stack.pop();
+        }
+        return Arrays.copyOfRange(array, 0, k);
+    }
+
+    public static void main(String[] args) {
+        MostCompetitive most = new MostCompetitive();
+        System.out.println(Arrays.toString(most.mostCompetitive_better(new int[]{2, 4, 3, 3, 5, 4, 9, 6}, 4)));
     }
 }
