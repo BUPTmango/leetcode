f969ea8f5219dc484b3af87394b98339ddc2a0a7
Guolong Wang <13121593633@163.com>
2020-12-03 15:37:28 +0800

    2020.12.3

--- src/hard_level/FindMedianSortedArrays.java
+++ src/hard_level/FindMedianSortedArrays.java
@@ -1,6 +1,5 @@
 package hard_level;
 
-import java.util.Arrays;
 
 /**
  * 4. 寻找两个正序数组的中位数
@@ -46,13 +45,20 @@ import java.util.Arrays;
  * @date 2020/12/3 9:53 上午
  */
 public class FindMedianSortedArrays {
+    /**
+     * 自己的做法 不满足题目要求的时间复杂度
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         // 合并两个有序数组
         int[] merge = merge(nums1, nums2);
         // 获取中位数
         int n = merge.length;
         if (n % 2 == 0) {
-            return (merge[n / 2 - 1] + merge[n / 2]) / 2;
+            return (merge[n / 2 - 1] + merge[n / 2]) / 2.0;
         } else {
             return merge[n / 2];
         }
@@ -79,8 +85,56 @@ public class FindMedianSortedArrays {
         return res;
     }
 
+    public double findMedianSortedArrays_better(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        if (m > n) {
+            // 保证 m <= n
+            return findMedianSortedArrays_better(B, A);
+        }
+        int iMin = 0, iMax = m;
+        while (iMin <= iMax) {
+            // 对小的数组进行二分
+            int i = (iMin + iMax) / 2;
+            // 对大的数组取剩下的部分
+            int j = (m + n + 1) / 2 - i;
+            // 说明j取多了 i 需要增大
+            if (j != 0 && i != m && B[j - 1] > A[i]) {
+                iMin = i + 1;
+                // 说明 i 取多了 i 需要减小
+            } else if (i != 0 && j != n && A[i - 1] > B[j]) {
+                iMax = i - 1;
+            } else { // 达到要求，并且将边界条件列出来单独考虑
+                int maxLeft = 0;
+                if (i == 0) {
+                    maxLeft = B[j - 1];
+                } else if (j == 0) {
+                    maxLeft = A[i - 1];
+                } else {
+                    maxLeft = Math.max(A[i - 1], B[j - 1]);
+                }
+                if ((m + n) % 2 == 1) {
+                    // 奇数的话不需要考虑右半部分
+                    return maxLeft;
+                }
+
+                int minRight = 0;
+                if (i == m) {
+                    minRight = B[j];
+                } else if (j == n) {
+                    minRight = A[i];
+                } else {
+                    minRight = Math.min(B[j], A[i]);
+                }
+                // 如果是偶数的话返回结果
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0.0;
+    }
+
     public static void main(String[] args) {
         FindMedianSortedArrays find = new FindMedianSortedArrays();
-        System.out.println(Arrays.toString(find.merge(new int[]{3, 5}, new int[]{2,4,6})));
+        System.out.println(find.findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}));
     }
 }
