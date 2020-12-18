82e290d217d6cf71f72415610f67d28253da5b1e
Guolong Wang <13121593633@163.com>
2020-12-18 15:26:24 +0800

    2020.12.18

--- src/simple_level/FindTheDifference.java
+++ src/simple_level/FindTheDifference.java
@@ -30,21 +30,61 @@ import java.util.Map;
  * @date 2020/9/4 9:09 上午
  */
 public class FindTheDifference {
+    /**
+     * 计数法
+     * @param s
+     * @param t
+     * @return
+     */
     public char findTheDifference(String s, String t) {
-        Map<Character, Integer> charMap = new HashMap<>();
-        for (char c : s.toCharArray()) {
-            charMap.put(c, charMap.getOrDefault(c, 0) + 1);
+        int[] cnt = new int[26];
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            cnt[ch - 'a']++;
         }
-
-        for (char ch : t.toCharArray()) {
-            if (!charMap.containsKey(ch)) {
+        for (int i = 0; i < t.length(); ++i) {
+            char ch = t.charAt(i);
+            cnt[ch - 'a']--;
+            if (cnt[ch - 'a'] < 0) {
                 return ch;
-            } else if (charMap.get(ch) == 0) {
-                return ch;
-            } else {
-                charMap.put(ch, charMap.get(ch) - 1);
             }
         }
-        return 'a';
+        return ' ';
+    }
+
+    /**
+     * 求和法 计算两个字符串的asc码的差值
+     * @param s
+     * @param t
+     * @return
+     */
+    public char findTheDifference_sum(String s, String t) {
+        int as = 0, at = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            as += s.charAt(i);
+        }
+        for (int i = 0; i < t.length(); ++i) {
+            at += t.charAt(i);
+        }
+        return (char) (at - as);
+    }
+
+    /**
+     * 位运算
+     * 如果将两个字符串拼接成一个字符串，则问题转换成求字符串中出现奇数次的字符。
+     * 类似于「136. 只出现一次的数字」，我们使用位运算的技巧解决本题。
+     * @param s
+     * @param t
+     * @return
+     */
+    public char findTheDifference_bit(String s, String t) {
+        int ret = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            ret ^= s.charAt(i);
+        }
+        for (int i = 0; i < t.length(); ++i) {
+            ret ^= t.charAt(i);
+        }
+        return (char) ret;
     }
 }
