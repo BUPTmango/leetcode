7f113a4160d7895ae5e013e526f0d4efb7c26b2e
Guolong Wang <13121593633@163.com>
2020-12-24 13:32:44 +0800

    2020.12.24

---
+++ src/hard_level/Candy.java
@@ -0,0 +1,61 @@
+package hard_level;
+
+/**
+ * 135. 分发糖果
+ * 老师想给孩子们分发糖果，有 N个孩子站成了一条直线，老师会根据每个孩子的表现，预先给他们评分。
+ *
+ * 你需要按照以下要求，帮助老师给这些孩子分发糖果：
+ *
+ * 每个孩子至少分配到 1 个糖果。
+ * 相邻的孩子中，评分高的孩子必须获得更多的糖果。
+ * 那么这样下来，老师至少需要准备多少颗糖果呢？
+ *
+ * 示例1:
+ *
+ * 输入: [1,0,2]
+ * 输出: 5
+ * 解释: 你可以分别给这三个孩子分发 2、1、2 颗糖果。
+ * 示例2:
+ *
+ * 输入: [1,2,2]
+ * 输出: 4
+ * 解释: 你可以分别给这三个孩子分发 1、2、1 颗糖果。
+ *      第三个孩子只得到 1 颗糖果，这已满足上述两个条件。
+ *
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2020/12/24 10:43 上午
+ */
+public class Candy {
+    /**
+     * 两次遍历
+     *
+     * 可以将「相邻的孩子中，评分高的孩子必须获得更多的糖果」这句话拆分为两个规则，分别处理。
+     * 左规则：当 ratings[i-1]<ratings[i] 时，i 号学生的糖果数量将比 i - 1 号孩子的糖果数量多。
+     * 右规则：当 ratings[i]>ratings[i+1] 时，i 号学生的糖果数量将比 i + 1 号孩子的糖果数量多。
+     *
+     * @param ratings
+     * @return
+     */
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] left = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && ratings[i] > ratings[i - 1]) {
+                left[i] = left[i - 1] + 1;
+            } else {
+                left[i] = 1;
+            }
+        }
+        int right = 0, ret = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (i < n - 1 && ratings[i] > ratings[i + 1]) {
+                right++;
+            } else {
+                right = 1;
+            }
+            ret += Math.max(left[i], right);
+        }
+        return ret;
+    }
+}
