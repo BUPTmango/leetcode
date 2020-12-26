e9de1605a89cda06c772100d3c63122877d8d20e
Guolong Wang <13121593633@163.com>
2020-12-26 16:36:21 +0800

    2020.12.26

--- src/hard_level/FreqStack.java
+++ src/hard_level/FreqStack.java
@@ -66,8 +66,9 @@ public class FreqStack {
     public void push(int x) {
         int f = freq.getOrDefault(x, 0) + 1;
         freq.put(x, f);
-        if (f > maxfreq)
+        if (f > maxfreq) {
             maxfreq = f;
+        }
 
         group.computeIfAbsent(f, z-> new Stack()).push(x);
     }
@@ -75,8 +76,9 @@ public class FreqStack {
     public int pop() {
         int x = group.get(maxfreq).pop();
         freq.put(x, freq.get(x) - 1);
-        if (group.get(maxfreq).size() == 0)
+        if (group.get(maxfreq).size() == 0) {
             maxfreq--;
+        }
         return x;
     }
 }
