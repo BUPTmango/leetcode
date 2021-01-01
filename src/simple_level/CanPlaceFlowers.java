3da0cb7bfafbd75cddeb43e6146d897c7ceaf9e2
Guolong Wang <13121593633@163.com>
2021-01-01 13:14:04 +0800

    2021.1.1

---
+++ src/simple_level/CanPlaceFlowers.java
@@ -0,0 +1,43 @@
+package simple_level;
+
+/**
+ * 605. 种花问题
+ * 假设你有一个很长的花坛，一部分地块种植了花，另一部分却没有。可是，花卉不能种植在相邻的地块上，它们会争夺水源，两者都会死去。
+ * <p>
+ * 给定一个花坛（表示为一个数组包含0和1，其中0表示没种植花，1表示种植了花），和一个数n。能否在不打破种植规则的情况下种入n朵花？能则返回True，不能则返回False。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: flowerbed = [1,0,0,0,1], n = 1
+ * 输出: True
+ * 示例 2:
+ * <p>
+ * 输入: flowerbed = [1,0,0,0,1], n = 2
+ * 输出: False
+ * 注意:
+ * <p>
+ * 数组内已种好的花不会违反种植规则。
+ * 输入的数组长度范围为 [1, 20000]。
+ * n 是非负整数，且不会超过输入数组的大小。
+ *
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2021/1/1 12:08 下午
+ */
+public class CanPlaceFlowers {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        // 每次跳两格
+        for (int i = 0; i < flowerbed.length; i += 2) {
+            // 如果当前为空地
+            if (flowerbed[i] == 0) {
+                // 如果是最后一格或者下一格为空
+                if (i == flowerbed.length - 1 || flowerbed[i + 1] == 0) {
+                    n--;
+                } else {
+                    i++;
+                }
+            }
+        }
+        return n <= 0;
+    }
+}
