83bc859d8bd826b574e5a84f87ecefaa9b3a7922
Guolong Wang <13121593633@163.com>
2021-01-05 18:53:14 +0800

    2021.1.5

--- src/middle_level/jianzhi_offer/CopyRandomList.java
+++ src/middle_level/jianzhi_offer/CopyRandomList.java
@@ -4,25 +4,25 @@ package middle_level.jianzhi_offer;
 /**
  * 剑指 Offer 35. 复杂链表的复制
  * 请实现 copyRandomList 函数，复制一个复杂链表。在复杂链表中，每个节点除了有一个 next 指针指向下一个节点，还有一个 random 指针指向链表中的任意节点或者 null。
- *
- *
+ * <p>
+ * <p>
  * 示例 1：
- *
- *
+ * <p>
+ * <p>
  * 输入：head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
  * 输出：[[7,null],[13,0],[11,4],[10,2],[1,0]]
  * 示例 2：
- *
- *
+ * <p>
+ * <p>
  * 输入：head = [[1,1],[2,1]]
  * 输出：[[1,1],[2,1]]
  * 示例 3：
- *
- *
+ * <p>
+ * <p>
  * 输入：head = [[3,null],[3,0],[3,null]]
  * 输出：[[3,null],[3,0],[3,null]]
  * 示例 4：
- *
+ * <p>
  * 输入：head = []
  * 输出：[]
  * 解释：给定的链表为空（空指针），因此返回 null。
@@ -33,17 +33,18 @@ package middle_level.jianzhi_offer;
  */
 public class CopyRandomList {
     public Node copyRandomList(Node head) {
-        if(head==null){
+        if (head == null) {
             return null;
         }
         copy(head);
         randomDirect(head);
         return reList(head);
     }
+
     // 拷贝链表
     // 复制每一个节点，使得复制后的节点都在当前节点的下一个节点
-    private void copy(Node head){
-        while(head!=null){
+    private void copy(Node head) {
+        while (head != null) {
             Node cloneNode = new Node(head.val);
             Node nextNode = head.next;
             head.next = cloneNode;
@@ -51,26 +52,29 @@ public class CopyRandomList {
             head = cloneNode.next;
         }
     }
+
     // 指定随机指针
     // 原生链表的节点的指向任意节点，使复制的节点也都指向某一任意节点
-    private void randomDirect(Node head){
-        while(head!=null){
+    private void randomDirect(Node head) {
+        while (head != null) {
             Node cloneNode = head.next;
-            if(head.random!=null){
+            if (head.random != null) {
                 Node direct = head.random;
                 cloneNode.random = direct.next;
             }
             head = cloneNode.next;
         }
     }
+
     // 重新连接 链表
     // 重新连接节点，把原生节点重新连接起来，把克隆后的节点连接起来
-    private Node reList(Node head){
+    private Node reList(Node head) {
         Node cloneNode = head.next;
         Node cloneHead = cloneNode;
+        // 注意！！！！ 这里注意要先走一次 才能防止空指针 让原来链表的在前面走
         head.next = cloneNode.next;
         head = head.next;
-        while(head!=null){
+        while (head != null) {
             cloneNode.next = head.next;
             head.next = head.next.next;
             head = head.next;
