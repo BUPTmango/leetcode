680842cfebe8bae0becb75ee46f02359a5a4ace0
Guolong Wang <13121593633@163.com>
2021-01-06 16:52:47 +0800

    最长递增子序列

---
+++ src/hard_level/MaxEnvelopes.java
@@ -0,0 +1,66 @@
+package hard_level;
+
+import java.util.Arrays;
+
+/**
+ * 最长递增子序列
+ * 相当于二维版本 要进行自定义排序预处理
+ * 354. 俄罗斯套娃信封问题
+ * 给定一些标记了宽度和高度的信封，宽度和高度以整数对形式(w, h)出现。当另一个信封的宽度和高度都比这个信封大的时候，这个信封就可以放进另一个信封里，如同俄罗斯套娃一样。
+ *
+ * 请计算最多能有多少个信封能组成一组“俄罗斯套娃”信封（即可以把一个信封放到另一个信封里面）。
+ *
+ * 说明:
+ * 不允许旋转信封。
+ *
+ * 示例:
+ *
+ * 输入: envelopes = [[5,4],[6,4],[6,7],[2,3]]
+ * 输出: 3
+ * 解释: 最多信封的个数为 3, 组合为: [2,3] => [5,4] => [6,7]。
+ *
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2021/1/6 4:37 下午
+ */
+public class MaxEnvelopes {
+    public int maxEnvelopes(int[][] envelopes) {
+        int n = envelopes.length;
+        // 按照w升序排序 如果w相同 按照h降序排序
+        Arrays.sort(envelopes, (o1, o2) -> o1[0] == o2[0] ? o2[1] - o1[1] : o1[0] - o2[0]);
+        // 获取高度数组
+        int[] height = new int[n];
+        for (int i = 0; i < n; i++) {
+            height[i] = envelopes[i][1];
+        }
+        // 按照正常的最长递增子序列的方法进行
+        return lengthOfLIS(height);
+    }
+
+    private int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        // dp[i]表示以nums[i]为结尾的最长上升子序列的长度
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                // 如果前面的数比当前的小 就考虑更新当前个数为小的数的个数加一
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+                }
+            }
+        }
+
+        int res = 1;
+        // 注意！！！ 这里不是返回最后一个元素 而是要遍历寻找
+        // 查找dp中以所有位置结尾的里面最长的序列
+        for (int i = 0; i < nums.length; i++) {
+            res = Math.max(res, dp[i]);
+        }
+
+        return res;
+    }
+}
