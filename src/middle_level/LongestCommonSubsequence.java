206601bcfa2bb006ce12a385cb27573808878fc5
Guolong Wang <13121593633@163.com>
2021-01-08 20:27:01 +0800

    最长公共子序列

--- src/middle_level/LongestCommonSubsequence.java
+++ src/middle_level/LongestCommonSubsequence.java
@@ -40,6 +40,7 @@ package middle_level;
 public class LongestCommonSubsequence {
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length(), n = text2.length();
+        // 对于text1[0..i - 1]和text2[0..j - 1] 他们的lcs长度是dp[i][j]
         int[][] dp = new int[m + 1][n + 1];
 
         // base case ： dp[0][0] = 0
