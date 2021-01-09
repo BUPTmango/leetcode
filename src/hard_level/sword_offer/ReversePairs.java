2e3e633776f1f54ba2b341c403995534c29f8680
Guolong Wang <13121593633@163.com>
2021-01-09 11:52:06 +0800

    2020.1.9

---
+++ src/hard_level/sword_offer/ReversePairs.java
@@ -0,0 +1,105 @@
+package hard_level.sword_offer;
+
+/**
+ * 剑指 Offer 51. 数组中的逆序对
+ * 在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。输入一个数组，求出这个数组中的逆序对的总数。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [7,5,6,4]
+ * 输出: 5
+ * <p>
+ * 限制：
+ * <p>
+ * 0 <= 数组长度 <= 50000
+ *
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2021/1/9 10:12 上午
+ */
+public class ReversePairs {
+    public int reversePairs(int[] nums) {
+        int len = nums.length;
+
+        // 连数对都构不成 更别说逆序了
+        if (len < 2) {
+            return 0;
+        }
+
+        int[] copy = new int[len];
+        for (int i = 0; i < len; i++) {
+            copy[i] = nums[i];
+        }
+
+        // 都使用一个临时数组 否则开销太大
+        int[] temp = new int[len];
+        return reversePairs(copy, 0, len - 1, temp);
+    }
+
+    /**
+     * nums[left..right] 计算逆序对个数并且排序
+     *
+     * @param nums
+     * @param left
+     * @param right
+     * @param temp
+     * @return
+     */
+    private int reversePairs(int[] nums, int left, int right, int[] temp) {
+        if (left == right) {
+            return 0;
+        }
+
+        int mid = left + (right - left) / 2;
+        int leftPairs = reversePairs(nums, left, mid, temp);
+        int rightPairs = reversePairs(nums, mid + 1, right, temp);
+
+        // 优化！！！ 如果已经有序 直接返回 不用merge
+        if (nums[mid] <= nums[mid + 1]) {
+            return leftPairs + rightPairs;
+        }
+
+        int crossPairs = mergeAndCount(nums, left, mid, right, temp);
+        return leftPairs + rightPairs + crossPairs;
+    }
+
+    /**
+     * nums[left..mid] 有序，nums[mid + 1..right] 有序
+     *
+     * @param nums
+     * @param left
+     * @param mid
+     * @param right
+     * @param temp
+     * @return
+     */
+    private int mergeAndCount(int[] nums, int left, int mid, int right, int[] temp) {
+        // merge的时候用temp当作额外空间
+        for (int i = left; i <= right; i++) {
+            temp[i] = nums[i];
+        }
+
+        int i = left;
+        int j = mid + 1;
+
+        int count = 0;
+        for (int k = left; k <= right; k++) {
+
+            if (i == mid + 1) {
+                nums[k] = temp[j];
+                j++;
+            } else if (j == right + 1) {
+                nums[k] = temp[i];
+                i++;
+            } else if (temp[i] <= temp[j]) {
+                nums[k] = temp[i];
+                i++;
+            } else {
+                nums[k] = temp[j];
+                j++;
+                count += (mid - i + 1);
+            }
+        }
+        return count;
+    }
+}
