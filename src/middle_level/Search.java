83653e727a9b0a90a2757e491f2d84169367fef5
Guolong Wang <13121593633@163.com>
2021-01-10 11:35:42 +0800

    搜索旋转排序数组

--- src/middle_level/Search.java
+++ src/middle_level/Search.java
@@ -3,21 +3,21 @@ package middle_level;
 /**
  * 33. 搜索旋转排序数组
  * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
- *
+ * <p>
  * ( 例如，数组?[0,1,2,4,5,6,7]?可能变为?[4,5,6,7,0,1,2]?)。
- *
+ * <p>
  * 搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回?-1?。
- *
+ * <p>
  * 你可以假设数组中不存在重复的元素。
- *
+ * <p>
  * 你的算法时间复杂度必须是?O(log?n) 级别。
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: nums = [4,5,6,7,0,1,2], target = 0
  * 输出: 4
  * 示例?2:
- *
+ * <p>
  * 输入: nums = [4,5,6,7,0,1,2], target = 3
  * 输出: -1
  *
@@ -31,6 +31,7 @@ public class Search {
      * middle分成的两边肯定有一边是有序的，通过端点可以判断哪一边有序
      * 有序那边的端点是max和min，可以判断target是不是在里面
      * 不在里面的话就去另一边寻找
+     *
      * @param nums
      * @param target
      * @return
@@ -41,8 +42,10 @@ public class Search {
         }
         int left = 0;
         int right = nums.length - 1;
-        int middle = left + (right-left) / 2;
+        // 可以发现二分搜索的两边肯定有一边是有序的
+        // 所以可以通过分别判断两边的各自的左右端点来查看具体哪一边是有序的
         while (left <= right) {
+            int middle = left + (right - left) / 2;
             if (nums[middle] == target) {
                 return middle;
             }
@@ -56,13 +59,12 @@ public class Search {
                     left = middle + 1;
                 }
             } else {
-                if(target >= nums[middle] && target <= nums[right]){
+                if (target >= nums[middle] && target <= nums[right]) {
                     left = middle + 1;
-                }else{
+                } else {
                     right = middle - 1;
                 }
             }
-            middle = left + (right-left) / 2;
         }
         return -1;
     }
