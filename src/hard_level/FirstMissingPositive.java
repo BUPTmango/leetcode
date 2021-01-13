a9b6cf4fdf1a8216e91959514a1f9ce4c02117ff
Guolong Wang <13121593633@163.com>
2021-01-13 20:57:32 +0800

    2020.1.13

--- src/hard_level/FirstMissingPositive.java
+++ src/hard_level/FirstMissingPositive.java
@@ -26,11 +26,17 @@ package hard_level;
  * @date 2020/12/14 8:13 下午
  */
 public class FirstMissingPositive {
+    /**
+     * 置换
+     * 在恢复后，数组应当有 [1, 2, ..., N] 的形式，但其中有若干个位置上的数是错误的，每一个错误的位置就代表了一个缺失的正数。
+     * @param nums
+     * @return
+     */
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
         // 每次交换会让一个元素到正确的位置上
         for (int i = 0; i < n; ++i) {
-            // 注意！！！！ 注意满足条件才交换
+            // 注意！！！！ 注意满足条件才交换 后面的判断是因为不一样才交换，防止死循环
             while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
                 int temp = nums[nums[i] - 1];
                 nums[nums[i] - 1] = nums[i];
