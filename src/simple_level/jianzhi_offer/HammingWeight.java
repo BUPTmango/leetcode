c1f7b7cfa062e4908e6ae9276d959246f2c99559
Guolong Wang <13121593633@163.com>
2021-01-14 20:35:03 +0800

    2020.1.14

--- src/simple_level/jianzhi_offer/HammingWeight.java
+++ src/simple_level/jianzhi_offer/HammingWeight.java
@@ -43,4 +43,13 @@ public class HammingWeight {
         }
         return res;
     }
+
+    public int hammingWeight_another(int n) {
+        int count = 0;
+        while (n != 0) {
+            count++;
+            n = (n - 1) & n;
+        }
+        return count;
+    }
 }
