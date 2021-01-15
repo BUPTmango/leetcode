c50e1f4633bda7af4f35f892c7573200d9dd104b
Guolong Wang <13121593633@163.com>
2021-01-15 11:52:18 +0800

    2020.1.15

--- src/middle_level/DeleteDuplicates.java
+++ src/middle_level/DeleteDuplicates.java
@@ -25,22 +25,20 @@ public class DeleteDuplicates {
         if (head == null || head.next == null) {
             return head;
         }
-        ListNode dummy = new ListNode(-1);
+        ListNode dummy = new ListNode(0);
         dummy.next = head;
-        ListNode a = dummy;
-        ListNode b = head;
-        while (b != null && b.next != null) {
-            // 初始化的时a指向的是哑结点，所以比较逻辑应该是a的下一个节点和b的下一个节点
-            if (a.next.val != b.next.val) {
-                a = a.next;
-                b = b.next;
-            } else {
-                // 如果a、b指向的节点值相等，就不断移动b，直到a、b指向的值不相等
-                while (b != null && b.next != null && a.next.val == b.next.val) {
-                    b = b.next;
+        ListNode prev = dummy, curr = head;
+        while (curr != null && curr.next != null) {
+            if (prev.next.val == curr.next.val) {
+                // 如果prev、curr指向的节点值相等，就不断移动curr，直到prev、curr指向的值不相等
+                while (curr != null && curr.next != null && prev.next.val == curr.next.val) {
+                    curr = curr.next;
                 }
-                a.next = b.next;
-                b = b.next;
+                prev.next = curr.next;
+                curr = curr.next;
+            } else {
+                prev = prev.next;
+                curr = curr.next;
             }
         }
         return dummy.next;
