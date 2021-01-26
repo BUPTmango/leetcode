f5764b62908816309fae4bba5f632eb9daba9ad4
Guolong Wang <13121593633@163.com>
2021-01-26 12:36:40 +0800

    单例模式

---
+++ src/interview/singleton/Enum.java
@@ -0,0 +1,17 @@
+package interview.singleton;
+
+import util.annotation.ThreadSafe;
+
+/**
+ * 枚举
+ *
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2021/1/26 12:30 下午
+ */
+@ThreadSafe("通过枚举类的特性满足线程安全")
+public enum Enum {
+    INSTANCE;
+    public void whateverMethod() {
+    }
+}
---
+++ src/interview/singleton/StaticInnerClass.java
@@ -0,0 +1,24 @@
+package interview.singleton;
+
+import util.annotation.ThreadSafe;
+
+/**
+ * 登记式/静态内部类
+ *
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2021/1/26 12:25 下午
+ */
+@ThreadSafe("利用了classloader 机制来保证初始化并且延迟加载")
+public class StaticInnerClass {
+    private static class SingletonHolder {
+        private static final StaticInnerClass SINGLETON = new StaticInnerClass();
+    }
+
+    private StaticInnerClass() {
+    }
+
+    public static final StaticInnerClass getInstance() {
+        return SingletonHolder.SINGLETON;
+    }
+}
