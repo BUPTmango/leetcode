7e27e614d8b43b6d347a59afca83185e76662467
Guolong Wang <13121593633@163.com>
2021-01-28 15:37:31 +0800

    修改快速排序

--- src/interview/sort/QuickSort.java
+++ src/interview/sort/QuickSort.java
@@ -11,8 +11,6 @@ import java.util.Arrays;
  */
 public class QuickSort {
 
-    private int count = 0;
-
     public void quickSort(int[] nums) {
         sort(nums, 0, nums.length - 1);
     }
@@ -27,7 +25,6 @@ public class QuickSort {
     }
 
     private int partition(int[] nums, int left, int right) {
-        count++;
         // 随机选一个作为pivot
         if (right > left) {
             int random = (int) (Math.random() * (right - left) + left + 1);
@@ -36,9 +33,6 @@ public class QuickSort {
             nums[random] = tmp;
         }
         int pivot = nums[left];
-        System.out.println();
-        System.out.println("准备开始partition : " + Arrays.toString(nums));
-        System.out.println("选中的pivot是 : " + pivot);
         while (left < right) {
             // nums[right] >= pivot 注意！！！！ 这里要有= 否则数组中有重复数字的时候会发生死循环
             while (left < right && nums[right] >= pivot) {
@@ -50,17 +44,14 @@ public class QuickSort {
             }
             nums[right] = nums[left];
         }
-        System.out.println("进行最后交换前 : " + Arrays.toString(nums));
         nums[left] = pivot;
-        System.out.println("partition结束 : " + Arrays.toString(nums));
         return left;
     }
 
     public static void main(String[] args) {
         QuickSort quickSort = new QuickSort();
-        int[] nums = new int[]{1,2,3,4,5,6,7,8};
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
         quickSort.quickSort(nums);
         System.out.println(Arrays.toString(nums));
-        System.out.println("一共进行了 " + quickSort.count + " 次partition.");
     }
 }
