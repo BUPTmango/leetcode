12e392cd1a5c226a9b527c9d134565ccb1cb3ca3
Guolong Wang <13121593633@163.com>
2021-06-02 09:40:31 +0800

    2021.6.2

---
+++ src/middle_level/CheckSubarraySum.java
@@ -0,0 +1,60 @@
+package middle_level;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 523. 连续的子数组和
+ * 给你一个整数数组 nums 和一个整数k ，编写一个函数来判断该数组是否含有同时满足下述条件的连续子数组：
+ * <p>
+ * 子数组大小 至少为 2 ，且
+ * 子数组元素总和为 k 的倍数。
+ * 如果存在，返回 true ；否则，返回 false 。
+ * <p>
+ * 如果存在一个整数 n ，令整数 x 符合 x = n * k ，则称 x 是 k 的一个倍数。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [23,2,4,6,7], k = 6
+ * 输出：true
+ * 解释：[2,4] 是一个大小为 2 的子数组，并且和为 6 。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [23,2,6,4,7], k = 6
+ * 输出：true
+ * 解释：[23, 2, 6, 4, 7] 是大小为 5 的子数组，并且和为 42 。
+ * 42 是 6 的倍数，因为 42 = 7 * 6 且 7 是一个整数。
+ * 示例 3：
+ * <p>
+ * 输入：nums = [23,2,6,4,7], k = 13
+ * 输出：false
+ *
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2021/6/2 09:10
+ */
+public class CheckSubarraySum {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int m = nums.length;
+        if (m < 2) {
+            return false;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int remainder = 0;
+        for (int i = 0; i < m; i++) {
+            // 差是k的倍数，表示二者除以k的余数相同
+            remainder = (remainder + nums[i]) % k;
+            if (map.containsKey(remainder)) {
+                int prevIndex = map.get(remainder);
+                if (i - prevIndex >= 2) {
+                    return true;
+                }
+            } else {
+                // 用hashmap记录每个余数第一次出现的下标
+                map.put(remainder, i);
+            }
+        }
+        return false;
+    }
+}
