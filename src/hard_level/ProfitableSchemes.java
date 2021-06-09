6480efabc94a826ec78c16e70a7a87082e455b44
Guolong Wang <13121593633@163.com>
2021-06-09 09:55:20 +0800

    2021.6.9

---
+++ src/hard_level/ProfitableSchemes.java
@@ -0,0 +1,48 @@
+package hard_level;
+
+/**
+ * 879. 盈利计划
+ * 集团里有 n 名员工，他们可以完成各种各样的工作创造利润。
+ * <p>
+ * 第i种工作会产生profit[i]的利润，它要求group[i]名成员共同参与。如果成员参与了其中一项工作，就不能参与另一项工作。
+ * <p>
+ * 工作的任何至少产生minProfit 利润的子集称为 盈利计划 。并且工作的成员总数最多为 n 。
+ * <p>
+ * 有多少种计划可以选择？因为答案很大，所以 返回结果模10^9 + 7的值。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：n = 5, minProfit = 3, group = [2,2], profit = [2,3]
+ * 输出：2
+ * 解释：至少产生 3 的利润，该集团可以完成工作 0 和工作 1 ，或仅完成工作 1 。
+ * 总的来说，有两种计划。
+ * 示例 2：
+ * <p>
+ * 输入：n = 10, minProfit = 5, group = [2,3,5], profit = [6,7,8]
+ * 输出：7
+ * 解释：至少产生 5 的利润，只要完成其中一种工作就行，所以该集团可以完成任何工作。
+ * 有 7 种可能的计划：(0)，(1)，(2)，(0,1)，(0,2)，(1,2)，以及 (0,1,2) 。
+ *
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2021/6/9 09:26
+ */
+public class ProfitableSchemes {
+    public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
+        // 多重背包
+        int[][] dp = new int[n + 1][minProfit + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        int len = group.length, MOD = (int) 1e9 + 7;
+        for (int i = 1; i <= len; i++) {
+            int members = group[i - 1], earn = profit[i - 1];
+            for (int j = n; j >= members; j--) {
+                for (int k = minProfit; k >= 0; k--) {
+                    dp[j][k] = (dp[j][k] + dp[j - members][Math.max(0, k - earn)]) % MOD;
+                }
+            }
+        }
+        return dp[n][minProfit];
+    }
+}
