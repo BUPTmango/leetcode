3239fd890c86a816996d06c49784ac2056ce2f38
Guolong Wang <13121593633@163.com>
2021-06-19 11:57:04 +0800

    2021.6.19

---
+++ src/middle_level/MaxLength.java
@@ -0,0 +1,76 @@
+package middle_level;
+
+import hard_level.Solution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 1239. 串联字符串的最大长度
+ * 给定一个字符串数组 arr，字符串 s 是将 arr 某一子序列字符串连接所得的字符串，如果 s 中的每一个字符都只出现过一次，那么它就是一个可行解。
+ * <p>
+ * 请返回所有可行解 s 中最长长度。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：arr = ["un","iq","ue"]
+ * 输出：4
+ * 解释：所有可能的串联组合是 "","un","iq","ue","uniq" 和 "ique"，最大长度为 4。
+ * 示例 2：
+ * <p>
+ * 输入：arr = ["cha","r","act","ers"]
+ * 输出：6
+ * 解释：可能的解答有 "chaers" 和 "acters"。
+ * 示例 3：
+ * <p>
+ * 输入：arr = ["abcdefghijklmnopqrstuvwxyz"]
+ * 输出：26
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= arr.length <= 16
+ * 1 <= arr[i].length <= 26
+ * arr[i]中只含有小写英文字母
+ *
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2021/6/19 11:39
+ */
+public class MaxLength {
+    int ans = 0;
+
+    public int maxLength(List<String> arr) {
+        List<Integer> masks = new ArrayList<Integer>();
+        for (String s : arr) {
+            int mask = 0;
+            for (int i = 0; i < s.length(); ++i) {
+                int ch = s.charAt(i) - 'a';
+                // 若 mask 已有 ch，则说明 s 含有重复字母，无法构成可行解
+                if (((mask >> ch) & 1) != 0) {
+                    mask = 0;
+                    break;
+                }
+                // 将 ch 加入 mask 中
+                mask |= 1 << ch;
+            }
+            if (mask > 0) {
+                masks.add(mask);
+            }
+        }
+
+        backtrack(masks, 0, 0);
+        return ans;
+    }
+
+    public void backtrack(List<Integer> masks, int pos, int mask) {
+        if (pos == masks.size()) {
+            ans = Math.max(ans, Integer.bitCount(mask));
+            return;
+        }
+        // mask 和 masks[pos] 无公共元素
+        if ((mask & masks.get(pos)) == 0) {
+            backtrack(masks, pos + 1, mask | masks.get(pos));
+        }
+        backtrack(masks, pos + 1, mask);
+    }
+}
