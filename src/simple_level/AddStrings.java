4c80fec14d92e82c53114739e64552f032f2c068
Guolong Wang <13121593633@163.com>
2021-07-24 22:36:59 +0800

    2021.7.24

--- src/simple_level/AddStrings.java
+++ src/simple_level/AddStrings.java
@@ -19,8 +19,10 @@ public class AddStrings {
     public String addStrings(String num1, String num2) {
         int i = num1.length() - 1, j = num2.length() - 1, carry = 0;
         StringBuilder ans = new StringBuilder();
+        // !!!!注意  还有carry != 0条件需要添加，因为有可能最后一位需要进位
         while (i >= 0 || j >= 0 || carry != 0) {
             // 不存在的进行补零
+            // 注意！！！ 这里不能直接用Integer.valueOf(),要用和'0'字符相减
             int x = i >= 0 ? num1.charAt(i) - '0' : 0;
             int y = j >= 0 ? num2.charAt(j) - '0' : 0;
             int result = x + y + carry;
