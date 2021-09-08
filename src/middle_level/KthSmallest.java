0567e26cf3362d7448416ccc88e395d5f14e864c
Guolong Wang <13121593633@163.com>
2021-09-08 18:53:55 +0800

    有序矩阵中第K小的元素

--- src/middle_level/KthSmallest.java
+++ src/middle_level/KthSmallest.java
@@ -1,10 +1,5 @@
 package middle_level;
 
-import data_structure.TreeNode;
-
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * 《玩转算法面试视频例题》二分搜索树
  * 378. 有序矩阵中第K小的元素
@@ -27,22 +22,33 @@ import java.util.List;
  * @date 2020/4/13 12:06 下午
  */
 public class KthSmallest {
-    List<Integer> list = new ArrayList<>();
-    public int kthSmallest(TreeNode root, int k) {
-        inOrder(root);
-        return list.get(k - 1);
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        int left = matrix[0][0];
+        int right = matrix[n - 1][n - 1];
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (check(matrix, mid, k, n)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 
-    /**
-     * 中序遍历
-     * @param node
-     */
-    private void inOrder(TreeNode node) {
-        if (node == null) {
-            return;
+    public boolean check(int[][] matrix, int mid, int k, int n) {
+        int i = n - 1;
+        int j = 0;
+        int num = 0;
+        while (i >= 0 && j < n) {
+            if (matrix[i][j] <= mid) {
+                num += i + 1;
+                j++;
+            } else {
+                i--;
+            }
         }
-        inOrder(node.left);
-        list.add(node.val);
-        inOrder(node.right);
+        return num >= k;
     }
 }
