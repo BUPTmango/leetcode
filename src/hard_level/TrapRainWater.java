669eaa9d87a9355ad548202b3ee18eec2a863ec0
Guolong Wang <13121593633@163.com>
2021-09-16 21:59:05 +0800

    接雨水 II

---
+++ src/hard_level/TrapRainWater.java
@@ -0,0 +1,74 @@
+package hard_level;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * 407. 接雨水 II
+ * 给你一个 m x n 的矩阵，其中的值均为非负整数，代表二维高度图每个单元的高度，请计算图中形状最多能接多少体积的雨水。
+ * <p>
+ * 示例 1:
+ * 输入: heightMap = [[1,4,3,1,3,2],[3,2,1,3,2,4],[2,3,3,2,3,1]]
+ * 输出: 4
+ * 解释: 下雨后，雨水将会被上图蓝色的方块中。总的接雨水量为1+2+1=4。
+ * <p>
+ * 示例2:
+ * 输入: heightMap = [[3,3,3,3,3],[3,2,2,2,3],[3,2,1,2,3],[3,2,2,2,3],[3,3,3,3,3]]
+ * 输出: 10
+ * <p>
+ * 提示:
+ * <p>
+ * m == heightMap.length
+ * n == heightMap[i].length
+ * 1 <= m, n <= 200
+ * 0 <= heightMap[i][j] <= 2 * 104
+ *
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2021/9/16 21:21
+ */
+public class TrapRainWater {
+    public int trapRainWater(int[][] heights) {
+        if (heights == null || heights.length == 0) {
+            return 0;
+        }
+        int n = heights.length;
+        int m = heights[0].length;
+        // 用一个vis数组来标记这个位置有没有被访问过
+        boolean[][] vis = new boolean[n][m];
+        // 优先队列中存放三元组 [x,y,h] 坐标和高度
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[2]));
+
+        // 先把最外一圈放进去
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 || i == n - 1 || j == 0 || j == m - 1) {
+                    pq.offer(new int[]{i, j, heights[i][j]});
+                    vis[i][j] = true;
+                }
+            }
+        }
+        int res = 0;
+        // 方向数组，把dx和dy压缩成一维来做
+        int[] dirs = {-1, 0, 1, 0, -1};
+        while (!pq.isEmpty()) {
+            int[] poll = pq.poll();
+            // 看一下周围四个方向，没访问过的话能不能往里灌水
+            for (int k = 0; k < 4; k++) {
+                int nx = poll[0] + dirs[k];
+                int ny = poll[1] + dirs[k + 1];
+                // 如果位置合法且没访问过
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && !vis[nx][ny]) {
+                    // 如果外围这一圈中最小的比当前这个还高，那就说明能往里面灌水
+                    if (poll[2] > heights[nx][ny]) {
+                        res += poll[2] - heights[nx][ny];
+                    }
+                    // 如果灌水高度得是你灌水后的高度了，如果没灌水也要取高的
+                    pq.offer(new int[]{nx, ny, Math.max(heights[nx][ny], poll[2])});
+                    vis[nx][ny] = true;
+                }
+            }
+        }
+        return res;
+    }
+}
