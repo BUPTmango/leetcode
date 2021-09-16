65b58a1398fbc6da4a117cc210f60eb5f7e93cba
Guolong Wang <13121593633@163.com>
2021-09-16 11:37:19 +0800

    LCS升级：求具体的子序列

--- src/middle_level/LongestCommonSubsequence.java
+++ src/middle_level/LongestCommonSubsequence.java
@@ -59,4 +59,43 @@ public class LongestCommonSubsequence {
         }
         return dp[m][n];
     }
+
+    /**
+     * 升级版 需要求具体的最长公共子序列是什么
+     */
+    public String LCS(String s1, String s2) {
+        int len1 = s1.length(), len2 = s2.length();
+        if (len1 == 0 || len2 == 0) {
+            return "-1";
+        }
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        int idx1 = len1, idx2 = len2;
+        while (idx1 > 0 && idx2 > 0) {
+            if (s1.charAt(idx1 - 1) == s2.charAt(idx2 - 1)) {
+                sb.append(s1.charAt(idx1 - 1));
+                idx1--;
+                idx2--;
+            } else {
+                if (dp[idx1 - 1][idx2] > dp[idx1][idx2 - 1]) {
+                    idx1--;
+                } else {
+                    idx2--;
+                }
+            }
+        }
+        if (sb.length() == 0) {
+            return "-1";
+        }
+        return sb.reverse().toString();
+    }
 }
