3a0352cef31694aa50f9d2319256b281a8f09a35
Guolong Wang <13121593633@163.com>
2021-09-19 17:52:24 +0800

    基本计算器

---
+++ src/hard_level/Calculate.java
@@ -0,0 +1,71 @@
+package hard_level;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 224. 基本计算器
+ * 给你一个字符串表达式 s ，请你实现一个基本计算器来计算并返回它的值。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "1 + 1"
+ * 输出：2
+ * 示例 2：
+ * <p>
+ * 输入：s = " 2-1 + 2 "
+ * 输出：3
+ * 示例 3：
+ * <p>
+ * 输入：s = "(1+(4+5+2)-3)+(6+8)"
+ * 输出：23
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 3* 105
+ * s 由数字、'+'、'-'、'('、')'、和 ' ' 组成
+ * s 表示一个有效的表达式
+ *
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2021/9/19 17:18
+ */
+public class Calculate {
+    /**
+     * 括号展开 + 栈
+     */
+    public int calculate(String s) {
+        Deque<Integer> ops = new LinkedList<Integer>();
+        ops.push(1);
+        int sign = 1;
+
+        int ret = 0;
+        int n = s.length();
+        int i = 0;
+        while (i < n) {
+            if (s.charAt(i) == ' ') {
+                i++;
+            } else if (s.charAt(i) == '+') {
+                sign = ops.peek();
+                i++;
+            } else if (s.charAt(i) == '-') {
+                sign = -ops.peek();
+                i++;
+            } else if (s.charAt(i) == '(') {
+                ops.push(sign);
+                i++;
+            } else if (s.charAt(i) == ')') {
+                ops.pop();
+                i++;
+            } else {
+                long num = 0;
+                while (i < n && Character.isDigit(s.charAt(i))) {
+                    num = num * 10 + s.charAt(i) - '0';
+                    i++;
+                }
+                ret += sign * num;
+            }
+        }
+        return ret;
+    }
+}
