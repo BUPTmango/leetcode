1747ac76205d34a6e659eff5906a9e07993842ec
Guolong Wang <13121593633@163.com>
2021-09-22 09:14:10 +0800

    基本计算器 支持加减乘除和括号

--- src/middle_level/Calculate.java
+++ src/middle_level/Calculate.java
@@ -1,5 +1,6 @@
 package middle_level;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.LinkedList;
 
@@ -60,6 +61,59 @@ public class Calculate {
                 }
                 preSign = s.charAt(i);
                 num = 0;
+            }
+        }
+        int ans = 0;
+        while (!stack.isEmpty()) {
+            ans += stack.pop();
+        }
+        return ans;
+    }
+
+    /**
+     * 如果支持加减乘除和括号
+     */
+    public int solve(String s) {
+        s = s.trim();
+        Deque<Integer> stack = new ArrayDeque<>();
+        int number = 0;
+        char sign = '+';
+        char[] charArray = s.toCharArray();
+        for (int i = 0, n = charArray.length; i < n; i++) {
+            char c = charArray[i];
+            if (c == ' ') {
+                continue;
+            }
+            if (Character.isDigit(c)) {
+                number = number * 10 + c - '0';
+            }
+            if (c == '(') {
+                int j = i + 1;
+                int counterPartition = 1;
+                while (counterPartition > 0) {
+                    if (charArray[j] == '(') {
+                        counterPartition++;
+                    }
+                    if (charArray[j] == ')') {
+                        counterPartition--;
+                    }
+                    j++;
+                }
+                number = solve(s.substring(i + 1, j - 1));
+                i = j - 1;
+            }
+            if (!Character.isDigit(c) || i == n - 1) {
+                if (sign == '+') {
+                    stack.push(number);
+                } else if (sign == '-') {
+                    stack.push(-1 * number);
+                } else if (sign == '*') {
+                    stack.push(stack.pop() * number);
+                } else if (sign == '/') {
+                    stack.push(stack.pop() / number);
+                }
+                number = 0;
+                sign = c;
             }
         }
         int ans = 0;
